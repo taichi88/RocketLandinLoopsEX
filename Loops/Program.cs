c7aefbac89e6dda297819fb02cad0a7f5ce4fa46
taichi88 <nikoloz.gabeskiria@gmail.com>
2024-12-02 14:18:56 +0400

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("CountDown for rocket launch ");
+/* For Loops 
+
+ */
+
+for (int counter = 15; counter >= 1; counter--)
+{
+    Console.WriteLine(counter + " Seconds left");
+    Thread.Sleep(1000);
+    Console.Clear();
+}
+
+Console.ReadLine();
